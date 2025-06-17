0a8b1013b2489cb36b0b25d3c799fafb30fa7150
SPARk574 <taiwoayo45@gmail.com>
2025-06-17 15:20:16 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,8 +10,8 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['923470027813'] //ur owner number
-global.ownernomer = "923470027813" //ur owner number2
+global.owner = ['09075221791'] //ur owner number
+global.ownernomer = "09075221791" //ur owner number2
 global.ownername = "🦄Dream Guy Xeon" //ur owner name
 global.ytname = "YT: xIKRATOSx" //ur yt chanel name
 global.socialm = "GitHub: xIKRATOSx" //ur github or insta name
@@ -19,9 +19,9 @@ global.location = "Pakistan, Punjab, Gujranwala" //ur location
 
 //new
 global.botname = "Cheems Bot MD V7"
-global.ownernumber = '923470027813'
+global.ownernumber = '09075221791'
 global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["923470027813@s.whatsapp.net"]
+global.ownerNumber = ["09075221791@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@xIKRATOSx"
 global.websitex = "https://youtube.com/@xIKRATOSx"
 global.wagc = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
